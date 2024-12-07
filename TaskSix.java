00cbdc2f27159adbf1217e85ffc7118d37b56d38
Ericallibalogun <allieric28@gmail.com>
2024-12-07 08:03:22 +0100

    the task six update

--- TaskSix.java
+++ TaskSix.java
@@ -2,13 +2,13 @@ public class TaskSix{
 public static void main(String... ericalli){
 	System.out.println("Powers of the multiples of 4 between 1 and 10: ");
 
-	for(int count = 1; count  < 11; count++)
-		if(count % 4 == 0){
-			System.out.printf("Powers of count" , count);
-			int result = count;
+	for(int number = 1; number < 11; number++)
+		if(number % 4 == 0){
+			System.out.printf("Powers of %d:%n" , number);
+			int result = 1;
 			for (int count = 1; count  <= 5; count++)
 				System.out.println(result);
-					result = result + count;
+					result *= number;
 
 
 }
